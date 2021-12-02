aa7aeb1844354a186cbd67e4c5f95e732f6dd068
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2021-12-02 22:06:34 +0100

    day02 2021

--- 2021/day02/day2.py
+++ 2021/day02/day2.py
@@ -1,3 +1,19 @@
 
 if __name__ == '__main__':
-    pass
+    xy = [0, 0]
+    xy2 = [0, 0, 0]
+    with open('day2.log') as log_file:
+        for line in log_file:
+            command, slope = line.split()
+            if command == 'forward':
+                xy[0] = xy[0] + int(slope)
+                xy2[0] = xy2[0] + int(slope)
+                xy2[1] = xy2[1] + int(slope) * xy2[2]
+            elif command == 'down':
+                xy[1] = xy[1] + int(slope)
+                xy2[2] = xy2[2] + int(slope)
+            elif command == 'up':
+                xy[1] = xy[1] - int(slope)
+                xy2[2] = xy2[2] - int(slope)
+    print(f'The final position is {xy} and thus the multiplied value is {xy[0] * xy[1]}.')
+    print(f'The final position is {xy2} and thus the multiplied value is {xy2[0] * xy2[1]}.')
