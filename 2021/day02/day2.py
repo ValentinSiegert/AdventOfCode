8980142a4f50772109c3d4b9924bb67bfda7173a
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2021-12-04 23:07:32 +0100

    Revert "reanming files input files from .log to .txt"
    This reverts commit a5ce7ef1540ba702e8ce40cfa5ea7ada2243f3e0.

--- 2021/day02/day2.py
+++ 2021/day02/day2.py
@@ -2,7 +2,7 @@
 if __name__ == '__main__':
     xy = [0, 0]  # horizontal position, depth
     xy2 = [0, 0, 0]  # horizontal position, depth, aim
-    with open('day2.txt') as log_file:
+    with open('day2.log') as log_file:
         for line in log_file:
             command, slope = line.split()
             if command == 'forward':
