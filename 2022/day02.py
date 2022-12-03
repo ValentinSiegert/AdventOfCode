b9ebac7804976937e8e103428b236d048b48c744
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2022-12-03 13:46:40 +0100

    2022 day 2
    adding short version

--- 2022/day02.py
+++ 2022/day02.py
@@ -16,5 +16,8 @@ if __name__ == '__main__':
             elif two == 3:
                 score += 6
             score += 3 if (one + two + 1) % 3 == 0 else (one + two + 1) % 3
-    print(f"Points: {points}")
-    print(f"Score: {score}")
+    print(f"Part1: {points}")
+    print(f"Part2: {score}")
+    # short version
+    print(f"Part1: {sum((3 + to) if (oo:= ord(o) - 64) == (to:= ord(t) - 87) else (6 + to) if (oo + 1) % 3 == to % 3 else (0 + to) for o, t in (l.strip().split() for l in open('day2.txt')))}")
+    print(f"Part2: {sum((3+(3 if ((fo:=ord(f)-64)+so+1)%3==0 else (fo+so+1)%3)) if (so:= ord(s) - 87) == 2 else (6 + (3 if ((fo:=ord(f)-64)+so+1)%3==0 else (fo+so+1)%3)) if so == 3 else (0 + (3 if ((fo:=ord(f)-64)+so+1)%3==0 else (fo+so+1)%3)) for f, s in (r.strip().split() for r in open('day2.txt')))}")
