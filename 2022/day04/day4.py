8efc7579f25a8b8cd1aa64d8a20b959c9858ad5f
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2022-12-04 14:46:05 +0100

    2022 day 4
    updated variable names for better readability

--- 2022/day04/day4.py
+++ 2022/day04/day4.py
@@ -4,9 +4,9 @@ if __name__ == '__main__':
     with open('day4.txt') as log_file:
         sub = overlap = 0
         for line in log_file:
-            sec1, sec2, sec3, sec4 = map(int, re.search(r'(\d*)-(\d*),(\d*)-(\d*)', line).groups())
-            sub += 1 if (sec1 <= sec3 and sec2 >= sec4) or (sec3 <= sec1 and sec4 >= sec2) else 0
-            overlap += 1 if sec1 <= sec4 and sec3 <= sec2 else 0
+            min1, max1, min2, max2 = map(int, re.search(r'(\d*)-(\d*),(\d*)-(\d*)', line).groups())
+            sub += 1 if (min1 <= min2 and max1 >= max2) or (min2 <= min1 and max2 >= max1) else 0
+            overlap += 1 if min1 <= max2 and min2 <= max1 else 0
     print(f"Part1: {sub}")
     print(f"Part2: {overlap}")
 
