8792fe6723876d4b5f3a793982082d3023953aeb
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2024-11-29 12:03:10 +0100

    adding aocd typer script

--- aoc.py
+++ aoc.py
@@ -1,5 +1,6 @@
+import datetime
 import os
-import subprocess
+import typer
 from pathlib import Path
 
 
@@ -7,6 +8,28 @@ from pathlib import Path
 CWD = Path(os.getcwd())
 AOCD_DIR = CWD / 'aocd'
 os.environ['AOCD_DIR'] = str(AOCD_DIR)
+TODAY = datetime.datetime.today()
+
+typer_app = typer.Typer()
+
+@typer_app.command()
+def fetch(year: int = int(TODAY.year), day: int = int(TODAY.day)):
+    puzzle = aocd.models.Puzzle(year=year, day=day)
+    if not (year_dir := (CWD / year)).exists():
+        os.makedirs(year_dir)
+        with open(year_dir / '__init__.py', 'w') as f:
+            f.write('')
+    if not (day_dir := (year_dir / f'day{day:02}')).exists():
+        os.makedirs(day_dir)
+        with open(day_dir / '__init__.py', 'w') as f:
+            f.write('')
+    # with open(day_dir / f'day{day:02}.txt', 'w') as f:
+    #     f.write(puzzle.input_data)
+
+@typer_app.command()
+def print_answer(year: int = int(TODAY.year), day: int = int(TODAY.day), part: int = 1):
+    puzzle = aocd.models.Puzzle(year=year, day=day)
+    print(f"Answer for year {year} day {day} part {part}: {puzzle.answers[part-1]}")
 
 
 if __name__ == "__main__":
@@ -16,8 +39,10 @@ if __name__ == "__main__":
         with open(AOCD_DIR / 'token', 'w') as f:
             f.write('your_token_here')
         print(f"Please add your token to the token file in the aocd directory and run the script again.")
-        print(f"Your token can be found at https://adventofcode.com/")
+        print(f"Your token can be found at https://adventofcode.com/ past logging in.")
         print(f"Place token to: {AOCD_DIR / 'token'}")
         exit(0)
+    import aocd
+    typer_app()
 
 
