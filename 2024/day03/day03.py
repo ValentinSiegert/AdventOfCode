a7eac07251c443537c58ddfbc84b9fa789e3abc7
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2024-12-03 11:07:14 +0100

    2024 day 3

---
+++ 2024/day03/day03.py
@@ -0,0 +1,27 @@
+import re
+
+def mul(a: int, b: int):
+    return a * b
+
+
+def part1(data: str):
+    return sum([eval(match) for match in re.findall(r"mul\(\d+,\d+\)", data)])
+
+def part2(data: str):
+    active, muls = True, []
+    for match in re.findall(r"mul\(\d+,\d+\)|don't\(\)|do\(\)", data):
+        if match == "don't()":
+            active = False
+        elif match == "do()":
+            active = True
+        elif active:
+            muls.append(eval(match))
+    return sum(muls)
+
+def solve(data: str, part: int):
+    if part == 1:
+        return part1(data)
+    if part == 2:
+        print(part2(data))
+        return part2(data)
+    return [part1(data), part2(data)]
