893af3dbd24d7dbb13c7daf9b4b2b76017088d3c
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2024-12-07 00:11:11 +0100

    2024 day 6
    redone without matrix rotation

--- 2024/day06/day06.py
+++ 2024/day06/day06.py
@@ -1,30 +1,38 @@
-import numpy as np
+def take_step(y: int, x: int, direction: str) -> tuple[int, int]:
+    if direction == "urdl":
+        return (y - 1, x)
+    if direction == "dlur":
+        return (y + 1, x)
+    if direction == "lurd":
+        return (y, x - 1)
+    if direction == "rdlu":
+        return (y, x + 1)
 
 
-def guard_pathing(data: str) -> tuple[int, int]:
-    replace_dict = {'\n': ';', '.': '0', '#': '1', '^': '2'}
-    field = np.rot90(np.matrix(",".join([replace_dict.get(c, c) for c in data]).replace(',;,', ';')), k=-1)
-    y, x = np.where(field == 2)
-    field[y, x] = 0
-    guard_in_field, unique_places = True, set()
-    while guard_in_field:
-        x += 1
-        if x[0] >= field.shape[0]:
-            guard_in_field = False
-            continue
-        if field[y, x] == 0:
-            unique_places.add((y[0], x[0]))
-        elif field[y, x] == 1:
-            x -= 1
-            field = np.rot90(field, k=1)
-            y, x = field.shape[0] - x - 1, y
-            unique_places = {(field.shape[0] - x1 - 1, y1) for y1, x1 in unique_places}
-    return len(unique_places), 0
+def guard_pathing(field: list[list[str]], y: int, x: int, d: str, obs: tuple[int, int] = None) -> tuple[set, bool]:
+    visited = set()
+    while 0 <= y < len(field) and 0 <= x < len(field[0]):
+        ny, nx = take_step(y, x, d)
+        if 0 <= ny < len(field) and 0 <= nx < len(field[0]) and (field[ny][nx] == '#' or (obs and (ny, nx) == obs)):
+            d = d[1:] + d[0]
+        else:
+            y, x = ny, nx
+        if (y, x, d) in visited:
+            return {(y, x) for y, x, _ in visited}, True
+        visited.add((y, x, d))
+    return {(y, x) for y, x, _ in visited}, False
 
 
 def solve(data: str, part: int):
+    field, direction = [list(line) for line in data.splitlines()], "urdl"
+    y, x = next((y, x) for y, row in enumerate(field) for x, cell in enumerate(row) if cell == "^")
+    visited, _ = guard_pathing(field, y, x, direction)
     if part == 1:
-        return guard_pathing(data)[0]
+        return len(visited) - 1
+    obstacle_amount, obstacles = 0, visited - {(y, x)}
+    for obs in obstacles:
+        if guard_pathing(field, y, x, direction, obs)[1]:
+            obstacle_amount += 1
     if part == 2:
-        return guard_pathing(data)[1]
-    return guard_pathing(data)
+        return obstacle_amount
+    return len(visited) - 1, obstacle_amount
