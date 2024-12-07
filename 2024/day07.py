d0c5a7b0ee18b222ea51a6feec716158faf0c9e1
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2024-12-07 15:58:03 +0100

    2024 day 7
    adding non brute force solution by checking each row backwards

--- 2024/day07.py
+++ 2024/day07.py
@@ -1,4 +1,8 @@
 import itertools
+from math import log10
+
+
+BRUTE_FORCE = False
 
 
 def calibrate(data: str, operators: list[str]) -> tuple[int, list[int]]:
@@ -19,13 +23,42 @@ def calibrate(data: str, operators: list[str]) -> tuple[int, list[int]]:
     return calibration, used_lines
 
 
+def solvable(result: int, numbers: list[int], concat: bool = False) -> bool:
+    *numbers, last = numbers
+    if not numbers:
+        return last == result
+    quant, rest = divmod(result, last)
+    if rest == 0 and solvable(quant, numbers, concat):
+        return True
+    if (concat and (result - last) % 10 ** (int(log10(last)) + 1) == 0
+        and solvable(result // 10 ** (int(log10(last)) + 1), numbers, concat)):
+        return True
+    return solvable(result - last, numbers, concat)
+
+
 def solve(data: str, part: int):
-    r1, used = calibrate(data, ['+', '*'])
-    if part == 1:
-        return r1
-    data = '\n'.join([line for i, line in enumerate(data.splitlines()) if i not in used])
-    r2, _ = calibrate(data, ['+', '*', '||'])
-    r2 += r1
-    if part == 2:
-        return r2
-    return [r1, r2]
+    if BRUTE_FORCE:
+        r1, used = calibrate(data, ['+', '*'])
+        if part == 1:
+            return r1
+        data = '\n'.join([line for i, line in enumerate(data.splitlines()) if i not in used])
+        r2, _ = calibrate(data, ['+', '*', '||'])
+        r2 += r1
+        if part == 2:
+            return r2
+        return [r1, r2]
+    else:
+        r1 = r2 = 0
+        for line in data.splitlines():
+            result, *numbers = line.split()
+            result, numbers = int(result[:-1]), list(map(int, numbers))
+            if solvable(result, numbers):
+                r1 += result
+            elif solvable(result, numbers, True):
+                r2 += result
+        r2 += r1
+        if part == 1:
+            return r1
+        if part == 2:
+            return r2
+        return [r1, r2]
