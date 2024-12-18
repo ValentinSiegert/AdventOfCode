4789ab1712a22eaa566e7d7edea824ae560e378f
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2024-12-18 08:34:28 +0100

    2024 day 17
    refactoring

--- 2024/day17.py
+++ 2024/day17.py
@@ -1,64 +1,49 @@
 import re
 
-COMBO_OPS = ['0', '1', '2', '3', 'reg_a', 'reg_b', 'reg_c',
+
+def execute(prog: list[int], reg_a: int, reg_b: int, reg_c: int, outs: list[int] | int) -> list[int] | int:
+    combo_ops = ['0', '1', '2', '3', 'reg_a', 'reg_b', 'reg_c',
                  '(lambda: exec("raise ValueError(\\"Combo operand 7 is reserved, but appeared in program.\\")"))()']
-
-
-def execute(prog: list[int], reg_a: int, reg_b: int, reg_c: int, outs: list[int], inst: int = 0) -> list[int]:
+    inst = 0
     while 0 <= inst < len(prog):
-        lit, com = prog[inst + 1], eval(COMBO_OPS[prog[inst + 1]])
         match prog[inst]:
             case 0:  # adv
-                reg_a = int(reg_a / 2 ** com)
+                reg_a >>= eval(combo_ops[prog[inst + 1]])
             case 1:  # bxl
-                reg_b ^= lit
+                reg_b ^= prog[inst + 1]
             case 2:  # bst
-                reg_b = com % 8
+                reg_b = eval(combo_ops[prog[inst + 1]]) & 7
             case 3:  # jnz
                 if reg_a != 0:
-                    inst = lit
-                    continue
+                    if type(outs) is list:
+                        inst = prog[inst + 1]
+                        continue
+                    else:
+                        raise ValueError('Operation jnz is not handled in non-list mode of part 2.')
             case 4:  # bxc
                 reg_b ^= reg_c
             case 5:  # out
-                outs.append(com % 8)
+                if type(outs) is list:
+                    outs.append(eval(combo_ops[prog[inst + 1]]) & 7)
+                else:
+                    return eval(combo_ops[prog[inst + 1]]) & 7
             case 6:  # bdv
-                reg_b = int(reg_a / 2 ** com)
+                reg_b = reg_a >> eval(combo_ops[prog[inst + 1]])
             case 7:  # cdv
-                reg_c = int(reg_a / 2 ** com)
+                reg_c = reg_a >> eval(combo_ops[prog[inst + 1]])
         inst += 2
     return outs
 
 
-def back_execute(prog: list[int], reg_a: int, reg_b: int, reg_c: int, prog_point: int, min_a: int) -> tuple[bool, int]:
+def find_a(prog: list[int], reg_b: int, reg_c: int, prog_point: int, min_a: int) -> tuple[bool, int]:
     out = -1
     if prog_point < 0:
         return True, min_a
     for bit in range(8):
-        reg_a, inst = min_a << 3 | bit, 0
-        while 0 <= inst < len(prog):
-            lit, com = prog[inst + 1], eval(COMBO_OPS[prog[inst + 1]])
-            match prog[inst]:
-                case 0:  # -adv
-                    reg_a >>= com
-                case 1:  # -bxl
-                    reg_b ^= lit
-                case 2:  # -bst
-                    reg_b = com & 7
-                case 3:  # -jnz
-                    inst = lit - 2 if reg_a != 0 else inst
-                case 4:  # -bxc
-                    reg_b ^= reg_c
-                case 5:  # -out
-                    out = com & 7
-                    break
-                case 6:  # -bdv
-                    reg_b = reg_a >> com
-                case 7:  # -cdv
-                    reg_c = reg_a >> com
-            inst += 2
+        reg_a = min_a << 3 | bit
+        execute(prog, reg_a, reg_b, reg_c, out)
         if out == prog[prog_point]:
-            solvable, ma = back_execute(prog, reg_a, reg_b, reg_c, prog_point - 1, min_a << 3 | bit)
+            solvable, ma = find_a(prog, reg_b, reg_c, prog_point - 1, min_a << 3 | bit)
             if solvable:
                 return True, ma
     return False, min_a
@@ -69,6 +54,6 @@ def solve(data: str, part: int):
                                 r'(?P<C>\d+)\s\sProgram:\s(?P<prog>[\d,]+)', data).groups()
     ra, rb, rc, prog = int(ra), int(rb), int(rc), list(map(int, prog.split(',')))
     if part == 1: return ','.join(map(str, execute(prog, ra, rb, rc, [])))
-    if part == 2: return back_execute(prog, ra, rb, rc, len(prog) - 1, 0)[1]
+    if part == 2: return find_a(prog, rb, rc, len(prog) - 1, 0)[1]
     return [','.join(map(str, execute(prog, ra, rb, rc, []))),
-            back_execute(prog, ra, rb, rc, len(prog) - 1, 0)[1]]
+            find_a(prog, rb, rc, len(prog) - 1, 0)[1]]
