c83884444b32de78709bb7941bce56a6555bb119
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2024-12-22 15:44:37 +0100

    adding basic time measuring

--- aoc.py
+++ aoc.py
@@ -34,7 +34,8 @@ def run(year: Annotated[int, typer.Option("--year", "-y")] = int(TODAY.year),
         day: Annotated[int, typer.Option("--day", "-d")] = int(TODAY.day),
         part: Annotated[int, typer.Option("--part", "-p")] = 0,
         input_data: Annotated[str, typer.Option("--input", "-i")] = "",
-        submit: Annotated[bool, typer.Option("--submit", "-s")] = False):
+        submit: Annotated[bool, typer.Option("--submit", "-s")] = False,
+        measure: Annotated[bool, typer.Option("--measure", "-m")] = False):
     """
     Run the solution for the given year and day.
     :param year: The year of the puzzle.
@@ -65,7 +66,17 @@ def run(year: Annotated[int, typer.Option("--year", "-y")] = int(TODAY.year),
         input_data = puzzle.examples[int(input_data)].input_data
     module = f'{year}.day{day:02}.day{day:02}'
     solution = __import__(module, fromlist=[''])
+    if measure:
+        import time
+        start = time.time()
     response = solution.solve(input_data, part)
+    if measure:
+        if part == 0:
+            print(f"{Color.BOLD}{Color.BLUE}Execution time for both parts:{Color.END} {time.time() - start:.4f} seconds")
+        if part == 1:
+            print(f"{Color.BOLD}{Color.BLUE}Execution time for part 1:{Color.END} {time.time() - start:.4f} seconds")
+        if part == 2:
+            print(f"{Color.BOLD}{Color.BLUE}Execution time for part 2:{Color.END} {time.time() - start:.4f} seconds")
     if not is_example_exec and submit:
         if part == 1:
             aocd.submit(response, part='a', day=day, year=year)
